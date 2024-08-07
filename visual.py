4d54f52568f39bd0beeeea463cc8b07778276b86
winterbreaker <joejamesdev@hotmail.com>
2024-08-07 00:27:07 -0700

    Update visual.py

--- visual.py
+++ visual.py
@@ -4,3 +4,10 @@ import matplotlib.pyplot as plt
 # %matplotlib inline
 import seaborn as sns
 print ("Setup complte")
+
+file = "melb_data.csv"
+data = pd.read_csv(file, index_col="Date", parse_dates=True)
+data.head()
+
+plt.figure(figsize=(16, 6))
+sns.lineplot(data = data)
